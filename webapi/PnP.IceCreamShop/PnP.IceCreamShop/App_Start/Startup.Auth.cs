f23d7afa39918923d377a9b5cd676ad7126e55ec
Velin Georgiev <velin.georgiev@pramerica.ie>
2019-05-14 17:28:57 +0300

    files added

---
+++ webapi/PnP.IceCreamShop/PnP.IceCreamShop/App_Start/Startup.Auth.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Microsoft.Owin.Security.ActiveDirectory;
+using System.IdentityModel.Tokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SharePoint.PnP.SampleRESTAPI
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    Tenant = ConfigurationManager.AppSettings["Domain"],
+                    TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidAudience = ConfigurationManager.AppSettings["Audience"]
+                    },
+                });
+        }
+    }
+}
